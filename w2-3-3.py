2eaa29a36cbb1049549b4113ee7b922fc49f3fe1
ecwu <bitlabwzh@gmail.com>
2017-01-26 19:15:10 +0800

    Guess number update

--- w2-3-3.py
+++ w2-3-3.py
@@ -1,17 +1,19 @@
 # Exercise: guess my number
-typeinnum = int(input("Please think of a number between 0 and 100!"))
+print("Please think of a number between 0 and 100!")
 guess = 50
+amnum=50
 print('Is your secret number ' + str(guess) + '?')
 usertypein = str(input("Enter 'h' to indicate the guess is too high. \
 Enter 'l' to indicate the guess is too low. \
 Enter 'c' to indicate I guessed correctly."))
 while usertypein != "c":
-	if usertypein == "h":
-		guess = int(guess/2)
-	else :
-		guess = int(guess + guess/2)
-	print('Is your secret number ' + str(guess) + '?')
-	usertypein = str(input("Enter 'h' to indicate the guess is too high. \
-	Enter 'l' to indicate the guess is too low. \
-	Enter 'c' to indicate I guessed correctly."))
-print('Game over. Your secret number was: ' + str(guess))
+    amnum = amnum/2
+    if usertypein == "h":
+        guess = guess-amnum
+    else :
+        guess = guess+amnum
+    print('Is your secret number ' + str(int(guess)) + '?')
+    usertypein = str(input("Enter 'h' to indicate the guess is too high. \
+    Enter 'l' to indicate the guess is too low. \
+    Enter 'c' to indicate I guessed correctly."))
+print('Game over. Your secret number was: ' + str(int(guess)))
