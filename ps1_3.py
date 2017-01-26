4e3fc6e42ae854c451b5e5a8c1983bd3514a62f4
ecwu <bitlabwzh@gmail.com>
2017-01-26 17:16:57 +0800

    Little Update

--- ps1_3.py
+++ ps1_3.py
@@ -1,5 +1,5 @@
-counter = 0
-counter2 = 0
+counter = int(0)
+counter2 = int(0)
 compare = "a"
 storage = ""
 for characters in s:
@@ -7,6 +7,7 @@ for characters in s:
 	while characters > compare:
 		counter2 += 1
 		compare = s[counter2]
-	storage = s[counter:counter2]
+	storage = s[int(counter):int(counter2)]
 	counter += 1
+	compare = "a"#Reset Compare
 print("Longest substring in alphabetical order is: " + str(storage))
