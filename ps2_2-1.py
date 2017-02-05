c397e37f10cbae9edea94c454879654ff1e51033
ecwu <bitlabwzh@gmail.com>
2017-02-06 00:01:59 +0800

    update code

--- ps2_2-1.py
+++ ps2_2-1.py
@@ -1,4 +1,5 @@
 def balances(minpay,balance,irt):
+    irt = 0
     while balance > 0:
         unpaid = balance - minpay
         interest = unpaid * monthlyPaymentRate
@@ -6,19 +7,25 @@ def balances(minpay,balance,irt):
         irt += 1
     return irt
 
-def times(bal):
+def times(bal,adda):
     irt = 0
     minpay = bal/12
     while irt != 12:
         irt = balances(minpay,bal,irt)
-        minpay += 1
+        minpay += adda
     return minpay
 
 balance = int(input('Input your balance'))
 annualInterestRate = float(input('Input Annual Interest Rate'))
 monthlyPaymentRate = annualInterestRate/12
 
+if balance < 10:
+    adda = 0.01
+elif balance < 100:
+    adda = 0.1
+else:
+    adda = 1
 
-print(str(times(balance)))
+print(str(times(balance,adda)))
 
 ##Use too much Resource
